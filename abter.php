05d3bbdbdb93c7fa0d24cc2948484f68181f5e76
Peter Aba <peter@peteraba.com>
2019-04-26 18:56:32 +0200

    Navbar to use dropdowns

--- abter.php
+++ abter.php
@@ -24,19 +24,19 @@ return [
     Module::EVENTS          => [
         Event::AUTH_READY            => [
             /** @see \AbterPhp\Files\Events\Listeners\AuthInitializer::handle */
-            sprintf('%s@handle', Events\Listeners\AuthInitializer::class),
+            Priorities::NORMAL => [sprintf('%s@handle', Events\Listeners\AuthInitializer::class)],
         ],
         Event::TEMPLATE_ENGINE_READY => [
             /** @see \AbterPhp\Files\Events\Listeners\TemplateInitializer::handle */
-            sprintf('%s@handle', Events\Listeners\TemplateInitializer::class),
+            Priorities::NORMAL => [sprintf('%s@handle', Events\Listeners\TemplateInitializer::class)],
         ],
         Event::NAVIGATION_READY      => [
             /** @see \AbterPhp\Files\Events\Listeners\NavigationBuilder::handle */
-            sprintf('%s@handle', Events\Listeners\NavigationBuilder::class),
+            Priorities::NORMAL => [sprintf('%s@handle', Events\Listeners\NavigationBuilder::class)],
         ],
         Event::DASHBOARD_READY       => [
             /** @see \AbterPhp\Files\Events\Listeners\DashboardBuilder::handle */
-            sprintf('%s@handle', Events\Listeners\DashboardBuilder::class),
+            Priorities::NORMAL => [sprintf('%s@handle', Events\Listeners\DashboardBuilder::class)],
         ],
     ],
     Module::ROUTE_PATHS     => [
--- src/Events/Listeners/NavigationBuilder.php
+++ src/Events/Listeners/NavigationBuilder.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace AbterPhp\Files\Events\Listeners;
 
 use AbterPhp\Files\Constant\Routes;
+use AbterPhp\Framework\Constant\Html5;
 use AbterPhp\Framework\Events\NavigationReady;
 use AbterPhp\Framework\Html\Component\ButtonFactory;
 use AbterPhp\Framework\I18n\ITranslator;
+use AbterPhp\Framework\Navigation\Dropdown;
 use AbterPhp\Framework\Navigation\Item;
 use AbterPhp\Framework\Navigation\Navigation;
 
@@ -39,15 +41,25 @@ class NavigationBuilder
             return;
         }
 
-        $this->addFileCategories($navigation);
-        $this->addFiles($navigation);
-        $this->addFileDownloads($navigation);
+        $dropdown = new Dropdown();
+        $dropdown[] = $this->createFileCategoriesItem();
+        $dropdown[] = $this->createFilesItem();
+        $dropdown[] = $this->createFileDownloadsItem();
+
+        $item   = $this->createFilesItem();
+        $item->setIntent(Item::INTENT_DROPDOWN);
+        $item->setAttribute(Html5::ATTR_ID, 'nav-files');
+        $item[0]->setAttribute(Html5::ATTR_HREF, 'javascript:void(0);');
+        $item[1] = $dropdown;
+
+        $navigation->addItem($item, static::BASE_WEIGHT);
     }
 
     /**
-     * @param Navigation $navigation
+     * @return Item
+     * @throws \Opulence\Routing\Urls\UrlException
      */
-    protected function addFileCategories(Navigation $navigation)
+    protected function createFileCategoriesItem(): Item
     {
         $text = 'files:fileCategories';
         $icon = 'folder';
@@ -55,13 +67,17 @@ class NavigationBuilder
         $button   = $this->buttonFactory->createFromName($text, Routes::ROUTE_FILE_CATEGORIES, [], $icon);
         $resource = $this->getAdminResource(Routes::ROUTE_FILE_CATEGORIES);
 
-        $navigation->addItem(new Item($button), static::BASE_WEIGHT, $resource);
+        $item = new Item($button);
+        $item->setResource($resource);
+
+        return $item;
     }
 
     /**
-     * @param Navigation $navigation
+     * @return Item
+     * @throws \Opulence\Routing\Urls\UrlException
      */
-    protected function addFiles(Navigation $navigation)
+    protected function createFilesItem(): Item
     {
         $text = 'files:files';
         $icon = 'attachment';
@@ -69,13 +85,17 @@ class NavigationBuilder
         $button   = $this->buttonFactory->createFromName($text, Routes::ROUTE_FILES, [], $icon);
         $resource = $this->getAdminResource(Routes::ROUTE_FILES);
 
-        $navigation->addItem(new Item($button), static::BASE_WEIGHT, $resource);
+        $item = new Item($button);
+        $item->setResource($resource);
+
+        return $item;
     }
 
     /**
-     * @param Navigation $navigation
+     * @return Item
+     * @throws \Opulence\Routing\Urls\UrlException
      */
-    protected function addFileDownloads(Navigation $navigation)
+    protected function createFileDownloadsItem(): Item
     {
         $text = 'files:fileDownloads';
         $icon = 'file_download';
@@ -83,7 +103,10 @@ class NavigationBuilder
         $button   = $this->buttonFactory->createFromName($text, Routes::ROUTE_FILE_DOWNLOADS, [], $icon);
         $resource = $this->getAdminResource(Routes::ROUTE_FILE_DOWNLOADS);
 
-        $navigation->addItem(new Item($button), static::BASE_WEIGHT, $resource);
+        $item = new Item($button);
+        $item->setResource($resource);
+
+        return $item;
     }
 
     /**
