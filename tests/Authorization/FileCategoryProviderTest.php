1614c95416a3c548673b5f9f5d8e76c4e6e4daba
Peter Aba <peter@peteraba.com>
2019-08-25 23:19:18 +0200

    Rename page category provider test to file category provider test

--- tests/Authorization/FileCategoryProviderTest.php
+++ tests/Authorization/FileCategoryProviderTest.php
@@ -10,7 +10,7 @@ use Casbin\Model\Model;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
-class PageCategoryProviderTest extends TestCase
+class FileCategoryProviderTest extends TestCase
 {
     /** @var FileCategoryProvider */
     protected $sut;
